64434eea393d380aeea195dde69a43bb59d77e00
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2024-05-03 00:07:06 +0200

    Added normalize css

---
+++ scripts/darkmode.js
@@ -0,0 +1,14 @@
+const modeButton = document.querySelector("#mode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        body.style.background = "#000";
+        body.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        body.style.background = "#eee";
+        body.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});
