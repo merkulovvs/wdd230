7820b2bb9ef5cd3a572ea9cc8481d0f0905d0440
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2024-05-04 10:21:15 +0200

    Week 2 Home page upgrade

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,14 +1,20 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
+const header = document.querySelector("header");
+const footer = document.querySelector("footer")
 
 modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🕶️")) {
+    if (modeButton.textContent.includes("⚫")) {
         body.style.background = "#000";
         body.style.color = "#fff";
-        modeButton.textContent = "🔆";
+        header.style.background = "#000";
+        footer.style.background = "#000"
+        modeButton.textContent = "⚪";
     } else {
         body.style.background = "#eee";
         body.style.color = "#000";
-        modeButton.textContent = "🕶️";
+        header.style.background = "#bff3e9";
+        footer.style.background = "#bff3e9";
+        modeButton.textContent = "⚫";
     }
 });
