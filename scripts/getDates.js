44fc92dd4f7673cdbe971d0d69c3a1cb36e0893d
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2024-05-30 23:01:11 +0200

    Week04 join page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,3 @@
 document.getElementById('lastModified').innerHTML = document.lastModified;
 const options = { year: "numeric" };
 document.querySelector('#currentYear').textContent = new Date().toLocaleDateString('de-DE', options);
-
-document.getElementById('datetimeStamp').innerHTML = document.lastModified;
