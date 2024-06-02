a2255c9102e54edbac7345e9451611f009683489
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2024-06-02 22:01:30 +0200

    Week05 dynamic links

--- scripts/links.js
+++ scripts/links.js
@@ -2,10 +2,23 @@ const baseURL = "https://merkulovvs.github.io/wdd230/";
 
 const linksURL = "https://merkulovvs.github.io/wdd230/data/links.json";
 
+const actvivities = document.querySelector('#activity-list');
+
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
 }
 
 getLinks();
+
+const displayLinks = (weeks) => {
+    weeks.forEach((entry) => {
+        let weekEntry = document.createElement('li');
+
+        weekEntry.textContent = `${entry.week}`;
+
+        actvivities.appendChild(weekEntry);
+
+    });
+}
