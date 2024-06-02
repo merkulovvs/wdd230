9aa0acbac01acb48e63314566948930c80d90d43
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2024-06-02 22:17:18 +0200

    Week05 dynamic links

--- scripts/links.js
+++ scripts/links.js
@@ -7,7 +7,7 @@ const actvivities = document.querySelector('#activity-list');
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.weeks);
 }
 
 getLinks();
@@ -15,10 +15,17 @@ getLinks();
 const displayLinks = (weeks) => {
     weeks.forEach((entry) => {
         let weekEntry = document.createElement('li');
+        let link = document.createElement('a');
 
-        weekEntry.textContent = `${entry.week}`;
+        weekEntry.textContent = `${entry.week}: `;
+        
+        link.setAttribute('href', entry.week.url);
+        link.textContent = `${entry.week.title}`;
 
+
+        weekEntry.appendChild(link);
         actvivities.appendChild(weekEntry);
+        
 
     });
 }
