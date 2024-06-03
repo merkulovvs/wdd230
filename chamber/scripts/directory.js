da083919352cac40c965991257c5ecbd49a6e49b
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2024-06-04 00:22:32 +0200

    Week05 directory update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -43,3 +43,17 @@ const displayMembers = (members) => {
     });
 
 }
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display =document.querySelector(".grid");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("grid");
+});
