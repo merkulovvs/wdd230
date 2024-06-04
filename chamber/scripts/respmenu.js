b1a936131299dcc9dbba4648d145375a3d34c77b
merkulovvs <18528771+merkulovvs@users.noreply.github.com>
2024-06-04 22:59:40 +0200

    nav links adjustment

--- chamber/scripts/respmenu.js
+++ chamber/scripts/respmenu.js
@@ -5,3 +5,15 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+	link.addEventListener('click', function () {
+		//Remove "active" class from all links
+		navLinks.forEach(link => link.classList.remove('active'));
+
+		//Add "active" class to the clicked link
+		this.classList.add('active');
+	});
+});
